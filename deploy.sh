bf1924b7461d0438638a8ff8ba72cc4a6211c5ae
weizongwei <weizongwei@didichuxing.com>
2016-05-13 15:24:31 +0800

    udpate book

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-cd ~/www/blog.hugo/ && rm -rf public && hugo --buildDrafts -t geo && cd ~/www/chungwei.github.io cp -R ~/www/blog.hugo/public/* .
+cd ~/www/blog.hugo/ && rm -rf public && hugo --buildDrafts -t geo && cd ~/www/chungwei.github.io && cp -R ~/www/blog.hugo/public/* .
