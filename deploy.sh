a3c64e190eecb90e7adff12bcfec5925e2e13dd5
chungwei_wei <wei.chungwei@gmail.com>
2016-05-14 11:32:45 +0800

    add file-git

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-cd ~/www/blog.hugo/ && rm -rf public && hugo --buildDrafts -t geo && cd ~/www/chungwei.github.io && cp -R ~/www/blog.hugo/public/* .
+cd ~/station/www/blog.hugo/ && rm -rf public && hugo --buildDrafts -t geo && cd ~/station/www/chungwei.github.io && cp -R ~/station/www/blog.hugo/public/* .
